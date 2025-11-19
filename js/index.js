2defb92abbf1a65237a83a2c2fa78e418e31d531
Serkan Sadulov <92607156+SerkanSadulov@users.noreply.github.com>
2025-11-19 13:50:27 +0200

    redesign

--- js/index.js
+++ js/index.js
@@ -1,26 +1,25 @@
-
 $(function () {
-
-  $('.navbar-collapse a').on('click', function () {
-    $(".navbar-collapse").collapse('hide');
+  $(".navbar-collapse a").on("click", function () {
+    $(".navbar-collapse").collapse("hide");
   });
 
   AOS.init({
-    disable: 'mobile',
+    disable: "mobile",
     duration: 800,
-    anchorPlacement: 'center-bottom'
+    anchorPlacement: "center-bottom",
   });
   $(function () {
-    $('.navbar a, .hero-text a').on('click', function (event) {
+    $(".navbar a, .hero-text a").on("click", function (event) {
       var $anchor = $(this);
-      $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top - 49
-      }, 1000);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $($anchor.attr("href")).offset().top - 49,
+          },
+          1000
+        );
       event.preventDefault();
     });
   });
 });
-
-
-
-
